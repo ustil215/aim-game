892fda10563139a5ccc25fe0ac0d67c95cfb0f4f
Ustil <ustilaantasher@yandex.ru>
2022-07-22 16:23:17 +0300

    Final

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const startBtn = document.querySelector('#start')
 const screens = document.querySelectorAll('.screen')
 const timeList = document.querySelector('#time-list')
 const timeEl = document.querySelector('#time')
-let time = 5
+const board = document.querySelector('#board')
+let time = 0
+let score = 0
 
 startBtn.addEventListener('click', (event) => {
    event.preventDefault()
@@ -17,24 +19,60 @@ timeList.addEventListener('click', (event) => {
    }
 })
 
+board.addEventListener('click', event => {
+   if (event.target.classList.contains('circle')) {
+      score++
+      event.target.remove()
+      createRandomCircle()
+   }
+})
 
-// DEBUG
-
-startGame()
 
 function startGame() {
    setInterval(decreaseTime, 1000)
+   createRandomCircle()
    setTime(time)
 }
 
 function decreaseTime() {
-   let current = --time
-   if (current < 10) {
-      current = `0${current}`
+   if (time === 0) {
+      finishGame()
+   } else {
+      let current = --time
+      if (current < 10) {
+         current = `0${current}`
+      }
+      setTime(current)
    }
-   setTime(current)
 }
 
 function setTime(value) {
    timeEl.innerHTML = `00:${value}`
+}
+
+function finishGame() {
+   timeEl.parentNode.classList.add('hide')
+   board.innerHTML = `<h1>Ваш счет: <span class="primary">${score}</span></h1>`
+}
+
+
+function createRandomCircle() {
+   const circle = document.createElement('div')
+
+   const size = getRandomNumber(10, 60)
+   const { width, height } = board.getBoundingClientRect()
+   const x = getRandomNumber(0, width - size)
+   const y = getRandomNumber(0, height - size)
+
+   circle.classList.add('circle')
+   circle.style.width = `${size}px`
+   circle.style.height = `${size}px`
+   circle.style.top = `${y}px`
+   circle.style.left = `${x}px`
+
+   board.append(circle)
+}
+
+function getRandomNumber(min, max) {
+   return Math.round(Math.random() * (max - min) + min)
 }
